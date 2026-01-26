2fce17f4052cba070585e8511e1c87fc94b7a000
Marcos Rodríguez <marcos.rdelgado@alumnos.upm.es>
2026-01-26 23:34:12 +0100

    Ficheros retundantes eliminados
    Se eliminan los ficheros redundantes del proyecto, y se eliminan las pares del poc marcadas.

--- poc/app.c
+++ poc/app.c
@@ -2,18 +2,15 @@
 #include "gpio.h"
 
 
-void SystemClock_Config(void);
 
 int main(void)
 {
-    // configuracion de HAL
     HAL_Init();
-    SystemClock_Config();
+    extern void SystemClock_Config();
     MX_GPIO_Init();
 
     while (1)
     {
-        // 
         HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
         HAL_Delay(500);
     }
